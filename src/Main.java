b7efdbc2dd8ddbef02735fc391707b68f5d29097
Eduardo Brayan <49373248+editcentralone@users.noreply.github.com>
2025-10-07 18:22:19 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+
+    public static void main(String[] args){
+    System.out.println("Este es mi primer programa en Java\n");
+        System.out.println("Este es mi primer programa en Java\n");
+    }
+    public void nombreMetodo (){
+
+    }
 }
